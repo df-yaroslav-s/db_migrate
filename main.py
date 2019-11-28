9e67fadf63a867508c28533dcbad43ccb33a0181
df-yaroslav-s <yaroslav_s@dataforest.ai>
2019-11-28 11:59:05 +0200

    just started

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import cx_Oracle
+
+class db_migrate:
+
+    def __init__(self, connection_string_from, connection_string_to, script):
+        self.connection_string_from = connection_string_from
+        self.connection_string_to = connection_string_to
+        self.script = script
+
+
+    def migrate (self):
+
 
